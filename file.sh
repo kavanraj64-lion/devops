a85e2867629521b6795cac7c54edc22af4c04ff4
kavanraj <kavanraj64@gmail.com>
2025-12-20 14:27:03 +0000

    this is jenkins

---
+++ file.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# installation of jenkins
+
+
+sudo apt update -y
+sudo apt install fontconfig openjdk-21-jre -y
+
+# long term support
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins -y
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins
